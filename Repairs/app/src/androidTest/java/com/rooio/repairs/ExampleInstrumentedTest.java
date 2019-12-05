f8224a47edc1006602e0603e82d8aa46704d24a3
lreckard <luke.reckard@gmail.com>
2019-12-04 17:26:15 -0800

    Delete test code for Login and MainActivity

--- Repairs/app/src/androidTest/java/com/rooio/repairs/ExampleInstrumentedTest.java
+++ Repairs/app/src/androidTest/java/com/rooio/repairs/ExampleInstrumentedTest.java
@@ -2,12 +2,25 @@ package com.rooio.repairs;
 
 import android.content.Context;
 
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -17,6 +30,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<Login> activityActivityTestRule = new ActivityTestRule(Login.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -24,4 +41,12 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.rooio.repairs", appContext.getPackageName());
     }
+
+    @Test
+    public void wrongUsername() {
+        onView(withId(R.id.username_field)).perform(typeText("hacker"));
+        onView(withId(R.id.login)).perform(click());
+        onView(withText("Incorrect Username and/or Password. Password must be at least 6 alphanumeric characters")).check(matches(isDisplayed()));
+    }
+    
 }
