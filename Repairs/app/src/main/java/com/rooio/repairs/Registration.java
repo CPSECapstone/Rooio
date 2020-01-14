0a196d8f5ccacb41a88f8c4896861b7d8da4fec6
Yusuf Bahadur <yusufbahadur@Yusufs-MacBook-Pro.local>
2020-01-14 13:17:05 -0800

    Reformatted Code

--- Repairs/app/src/main/java/com/rooio/repairs/Registration.java
+++ Repairs/app/src/main/java/com/rooio/repairs/Registration.java
@@ -27,15 +27,23 @@ public class Registration extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        //Input initialization;
         myspinner = (Spinner) findViewById(R.id.spinner);
         register = (Button) findViewById(R.id.register);
+        firstname = (EditText) findViewById(R.id.firstname);
+        lastname = (EditText) findViewById(R.id.lastname);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        restaurantname = (EditText) findViewById(R.id.restaurantname);
 
+        //Functions for DropDown "Industry Type"
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String >(Registration.this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.industry_type));
         //Set Adapter as
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         myspinner.setAdapter(myAdapter);
 
+        //Functions for the "Register Button" to Login Page
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
