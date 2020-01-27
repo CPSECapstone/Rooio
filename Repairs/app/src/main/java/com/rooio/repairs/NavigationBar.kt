126c0630cfd5ad774573f220e1e5d67c280a6701
ksunjara <ksunny129@yahoo.com>
2020-01-26 16:10:26 -0800

    updated version of icons switching

--- Repairs/app/src/main/java/com/rooio/repairs/NavigationBar.kt
+++ Repairs/app/src/main/java/com/rooio/repairs/NavigationBar.kt
@@ -33,41 +33,14 @@ abstract class NavigationBar : AppCompatActivity() {
         val settingsText = transitionsContainer.findViewById<TextView>(R.id.settings_text)
         val collapse = transitionsContainer.findViewById<ImageView>(R.id.collapse)
         val divider = transitionsContainer.findViewById<View>(R.id.nav_divider)
-        val expand = transitionsContainer.findViewById<ImageView>(R.id.expand)
         val dashboardImage = transitionsContainer.findViewById<ImageView>(R.id.dashboard)
         val settingsImage = transitionsContainer.findViewById<ImageView>(R.id.settings)
 
-        expand.setOnClickListener{
-            expand.visibility = View.INVISIBLE
-            collapse.visibility = View.VISIBLE
-
-            TransitionManager.beginDelayedTransition(transitionsContainer)
-            visible = !visible
-            val v = if (visible) View.GONE else View.VISIBLE
-            collapseText.visibility = v
-            dashboardText.visibility = v
-            jobsText.visibility = v
-            equipmentText.visibility = v
-            settingsText.visibility = v
-
-
-            //changing width of the side navigation bar
-            val params = navBar.layoutParams
-            val p = if (visible) 65 else 250
-            params.width = p
-
-            //changing width of horizontal divider
-            val params2 = divider.layoutParams
-            val p2 = if (visible) 65 else 250
-            params2.width = p2
-
-            animateActivity(visible)
-
-        }
+        TransitionManager.beginDelayedTransition(transitionsContainer)
 
         collapse.setOnClickListener {
 
-            TransitionManager.beginDelayedTransition(transitionsContainer)
+
             visible = !visible
             val v = if (visible) View.GONE else View.VISIBLE
             collapseText.visibility = v
@@ -75,9 +48,9 @@ abstract class NavigationBar : AppCompatActivity() {
             jobsText.visibility = v
             equipmentText.visibility = v
             settingsText.visibility = v
+            val rotate = if (visible) 180f else 0f
+            collapse.setRotation(rotate)
 
-            collapse.visibility = View.INVISIBLE
-            expand.visibility = View.VISIBLE
 
 
             val params = navBar.layoutParams
