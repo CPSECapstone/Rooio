49f1ea996baeae15416d0d280d3ba7b97eb572f9
Jessica Chang <jessicachang38@gmail.com>
2020-02-20 14:27:15 -0800

    Update EquipmentActivityTest.kt

--- Repairs/app/src/test/java/com/rooio/repairs/EquipmentActivityTest.kt
+++ Repairs/app/src/test/java/com/rooio/repairs/EquipmentActivityTest.kt
@@ -1,2 +1,23 @@
 package com.rooio.repairs
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+
+@RunWith(AndroidJUnit4::class)
+class EquipmentActivityTest{
+
+    private var activity: Equipment? = null
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        activity = Robolectric.buildActivity(Equipment::class.java)
+                .create()
+                .resume()
+                .get()
+    }
+
+
+}
