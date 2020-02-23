1f439706652712a0f81368871297c75a28d0f94c
ksunjara <ksunny129@yahoo.com>
2020-02-23 14:53:14 -0800

    searchbar working

--- Repairs/app/src/main/java/com/rooio/repairs/ChooseEquipment.kt
+++ Repairs/app/src/main/java/com/rooio/repairs/ChooseEquipment.kt
@@ -3,26 +3,32 @@ package com.rooio.repairs
 import android.os.Bundle
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import android.widget.EditText
-import android.widget.TextView
 import android.view.LayoutInflater
 import android.content.Context;
 import android.view.View;
-import android.widget.Filter;
-import android.widget.Filterable;
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.*
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import org.json.JSONArray
+import androidx.arch.core.util.Function
+import org.json.JSONException
 
 
 class ChooseEquipment : RestApi() {
 
+    val url = "https://capstone.api.roopairs.com/v0/service-locations/$userLocationID/equipment/"
     private var list: RecyclerView? = null
     private var search: EditText? = null
     private var recyclerAdapter: adapter? = null
-    var country: ArrayList<String>? = null
-    var listcountry: ArrayList<String>? = null
+    //private var country: ArrayList<String>? = null
+    private var country = ArrayList<String>()
+    private lateinit var textView: TextView
+    //var listcountry: ArrayList<String>? = null
+    private var equipmentObjectList = ArrayList<EquipmentData>()
+    private var equipmentList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,16 +41,10 @@ class ChooseEquipment : RestApi() {
         supportActionBar!!.elevation = 0.0f
 
         list = findViewById(R.id.list) as RecyclerView
+        textView = findViewById(R.id.errorText)
         search = findViewById(R.id.search) as EditText
-        listcountry = getListCountry()
         country = ArrayList()
-        country!!.addAll(listcountry!!)
-        val layoutManager = LinearLayoutManager(this)
-        list!!.setLayoutManager(layoutManager)
-        recyclerAdapter = adapter(this@ChooseEquipment, country!!)
-        list!!.addItemDecoration(DividerItemDecoration(list!!.getContext(), layoutManager.orientation))
-        list!!.setAdapter(recyclerAdapter)
-
+        loadEquipmentElements()
 
             search!!.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
@@ -54,7 +54,65 @@ class ChooseEquipment : RestApi() {
 
                 override fun afterTextChanged(editable: Editable) {}
             })
+    }
+
+
+
+    // send JsonRequest Object
+    private fun loadEquipmentElements() {
+        val equipmentType = intent.getIntExtra("equipmentType", 0)
+        val request = JsonRequest(false, url, null, responseFuncLoad, errorFuncLoad, true)
+        requestGetJsonArray(request)
+    }
+
+    @JvmField
+    // load equipments in the equipment list
+    var responseFuncLoad = Function<Any, Void?> { jsonResponse: Any? ->
+        try {
+            val jsonArray = jsonResponse as JSONArray
+            loadEquipment(jsonArray)
+        } catch (e: JSONException) {
+            e.printStackTrace()
         }
+        null
+    }
+
+    @JvmField
+    // set error message
+    var errorFuncLoad = Function<String, Void?> { string: String? ->
+        textView.text = string
+        textView.setTextColor(ContextCompat.getColor(this,R.color.Red))
+        null
+    }
+
+    // getting all the equipment for the equipment list
+    // passing equipment list to custom adapter
+    private fun loadEquipment(response: JSONArray) {
+        equipmentObjectList.clear()
+        //equipmentList.clear()
+        for (i in 0 until response.length()) {
+            val equipment = EquipmentData(response.getJSONObject(i))
+            equipmentObjectList.add(equipment)
+        }
+
+        equipmentObjectList.sortWith(compareBy {it.location})
+
+        for (i in 0 until equipmentObjectList.size)
+        {
+            equipmentList.add(equipmentObjectList[i].name)
+        }
+
+        country.addAll(equipmentList)
+        val layoutManager = LinearLayoutManager(this)
+        list!!.setLayoutManager(layoutManager)
+        recyclerAdapter = adapter(this@ChooseEquipment, country!!)
+        list!!.addItemDecoration(DividerItemDecoration(list!!.getContext(), layoutManager.orientation))
+        list!!.setAdapter(recyclerAdapter)
+
+//        customAdapter = EquipmentCustomAdapter(this, equipmentList)
+//        if (equipmentList.size != 0) equipmentListView.adapter = customAdapter
+
+    }
 
         private inner class adapter(internal var context: Context, internal var mData: List<String>) : RecyclerView.Adapter<adapter.myViewHolder>(), Filterable {
             internal var mfilter: NewFilter
@@ -94,10 +152,10 @@ class ChooseEquipment : RestApi() {
                     country!!.clear()
                     val results = FilterResults()
                     if (charSequence.length == 0) {
-                        country!!.addAll(listcountry!!)
+                        country!!.addAll(equipmentList!!)
                     } else {
                         val filterPattern = charSequence.toString().toLowerCase().trim { it <= ' ' }
-                        for (listcountry in listcountry!!) {
+                        for (listcountry in equipmentList!!) {
                             if (listcountry.toLowerCase().startsWith(filterPattern)) {
                                 country!!.add(listcountry)
                             }
@@ -113,21 +171,6 @@ class ChooseEquipment : RestApi() {
                 }
 
             }
-        }
-
-        fun getListCountry(): ArrayList<String> {
-            val listcountry = arrayListOf<String>()
-            listcountry.add("Nigeria")
-            listcountry.add("China")
-            listcountry.add("USA")
-            listcountry.add("Ghana")
-            listcountry.add("Canada")
-            listcountry.add("Finland")
-            listcountry.add("Denmark")
-            listcountry.add("Argentina")
-            listcountry.add("Andorra")
-            listcountry.add("Togo")
-            return listcountry
         }
 
     }
