3591de9a806a63b95d637825cfe418fe901c4526
ksunjara <ksunny129@yahoo.com>
2020-02-23 23:43:34 -0800

    updates

--- Repairs/app/src/main/java/com/rooio/repairs/ChooseEquipment.kt
+++ Repairs/app/src/main/java/com/rooio/repairs/ChooseEquipment.kt
@@ -5,8 +5,11 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.content.Context;
+import android.content.Intent
+import android.graphics.Canvas
 import android.view.View;
 import android.text.Editable
+import android.text.Layout
 import android.text.TextWatcher
 import android.widget.*
 import androidx.core.content.ContextCompat
@@ -14,11 +17,36 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.json.JSONArray
 import androidx.arch.core.util.Function
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.transition.TransitionManager
 import org.json.JSONException
+
+
+
+
+
 
 
 class ChooseEquipment : RestApi() {
 
+    private lateinit var equipmentName: TextView
+    private lateinit var manufacturer: TextView
+    private lateinit var modelNumber: TextView
+    private lateinit var location: TextView
+    private lateinit var serialNumber: TextView
+    private lateinit var lastServiceBy: TextView
+    private lateinit var lastServiceDate: TextView
+    private lateinit var manufacturerText: TextView
+    private lateinit var modelText: TextView
+    private lateinit var locationText: TextView
+    private lateinit var serialText: TextView
+    private lateinit var lastServiceByText: TextView
+    private lateinit var lastServiceDateText: TextView
+    private lateinit var dropDown: ImageView
+    private lateinit var equipmentDivider: ImageView
+    private lateinit var equipmentLayout: ConstraintLayout
+    private lateinit var viewEquipment: TextView
+    private lateinit var transitionsContainer: ViewGroup
     val url = "https://capstone.api.roopairs.com/v0/service-locations/$userLocationID/equipment/"
     private var list: RecyclerView? = null
     private var search: EditText? = null
@@ -29,6 +57,7 @@ class ChooseEquipment : RestApi() {
     //var listcountry: ArrayList<String>? = null
     private var equipmentObjectList = ArrayList<EquipmentData>()
     private var equipmentList = ArrayList<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +75,11 @@ class ChooseEquipment : RestApi() {
         country = ArrayList()
         loadEquipmentElements()
 
+        list!!.addItemDecoration(object : DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL) {
+            override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+                // Do not draw the divider
+            }
+        })
             search!!.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
                 override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
@@ -58,7 +92,7 @@ class ChooseEquipment : RestApi() {
 
 
 
-    // send JsonRequest Object
+        // send JsonRequest Object
     private fun loadEquipmentElements() {
         val equipmentType = intent.getIntExtra("equipmentType", 0)
         val request = JsonRequest(false, url, null, responseFuncLoad, errorFuncLoad, true)
@@ -106,7 +140,6 @@ class ChooseEquipment : RestApi() {
         val layoutManager = LinearLayoutManager(this)
         list!!.setLayoutManager(layoutManager)
         recyclerAdapter = adapter(this@ChooseEquipment, country!!)
-        list!!.addItemDecoration(DividerItemDecoration(list!!.getContext(), layoutManager.orientation))
         list!!.setAdapter(recyclerAdapter)
 
 //        customAdapter = EquipmentCustomAdapter(this, equipmentList)
@@ -115,6 +148,7 @@ class ChooseEquipment : RestApi() {
     }
 
         private inner class adapter(internal var context: Context, internal var mData: List<String>) : RecyclerView.Adapter<adapter.myViewHolder>(), Filterable {
+
             internal var mfilter: NewFilter
 
             override fun getFilter(): Filter {
@@ -132,7 +166,24 @@ class ChooseEquipment : RestApi() {
             }
 
             override fun onBindViewHolder(holder: adapter.myViewHolder, position: Int) {
+                setVisibility(holder, View.GONE)
+                val initial = holder.equipmentLayout.layoutParams
+                initial.height = 90
                 holder.country.text = mData[position]
+                holder.dropDown.setOnClickListener{
+                    TransitionManager.beginDelayedTransition(holder.transitionsContainer)
+                    holder.visible = !holder.visible
+                    val v = if (holder.visible) View.VISIBLE else View.GONE
+                    val op = if (holder.visible) View.GONE else View.VISIBLE
+                    //viewEquipment.visibility = op
+                    setVisibility(holder, v)
+                    val rotate = if (holder.visible) 180f else 0f
+                    holder.dropDown.rotation = rotate
+                    val params = holder.equipmentLayout.layoutParams
+                    val p = if (holder.visible) 395 else 90
+                    params.height = p
+                }
+
             }
 
             override fun getItemCount(): Int {
@@ -141,28 +192,106 @@ class ChooseEquipment : RestApi() {
 
             inner class myViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
                 internal var country: TextView
+                internal var dropDown: ImageView
+                internal var equipmentLayout : ConstraintLayout
+                internal var transitionsContainer : ViewGroup
+                internal var visible: Boolean = false
+                internal var manufacturerText : TextView
+                internal var modelText : TextView
+                internal var locationText: TextView
+                internal var serialText: TextView
+                internal var lastServiceByText: TextView
+                internal var lastServiceDateText: TextView
+                internal var manufacturer: TextView
+                internal var modelNumber: TextView
+                internal var location: TextView
+                internal var serialNumber: TextView
+                internal var lastServiceBy: TextView
+                internal var lastServiceDate: TextView
+                internal var equipmentDivider: ImageView
+
 
                 init {
-                    country = itemView.findViewById(R.id.country)
-                }
-            }
+                    country = itemView.findViewById(R.id.equipmentName)
+                    transitionsContainer = itemView.findViewById(R.id.relativeLayout)
+                    equipmentLayout = transitionsContainer.findViewById(R.id.equipmentLayout)
+//                    equipmentName = transitionsContainer.findViewById(R.id.equipmentName)
+                    dropDown = transitionsContainer.findViewById(R.id.dropDown)
+                    manufacturer = transitionsContainer.findViewById(R.id.manufacturerInfo)
+                    modelNumber = transitionsContainer.findViewById(R.id.modelInfo)
+                    location = transitionsContainer.findViewById(R.id.locationInfo)
+                    serialNumber = transitionsContainer.findViewById(R.id.serialInfo)
+                    lastServiceBy = transitionsContainer.findViewById(R.id.lastServiceByInfo)
+                    lastServiceDate = transitionsContainer.findViewById(R.id.lastServiceDateInfo)
+                    manufacturerText = transitionsContainer.findViewById(R.id.manufacturerText)
+                    modelText = transitionsContainer.findViewById(R.id.modelText)
+                    locationText = transitionsContainer.findViewById(R.id.locationText)
+                    serialText = transitionsContainer.findViewById(R.id.serialText)
+                    lastServiceByText = transitionsContainer.findViewById(R.id.lastServiceByText)
+                    lastServiceDateText = transitionsContainer.findViewById(R.id.lastServiceDateText)
+                    equipmentDivider = transitionsContainer.findViewById(R.id.equipmentDivider)
 
+
+//                    viewEquipment = transitionsContainer.findViewById(R.id.viewEquipment)
+                }
+
+//                fun recyclerAnimation(): (View) -> Unit = {
+//                    layoutPosition.also {
+//                        var visible = false
+//                        setVisibility(View.GONE)
+//                        val initial = equipmentLayout.layoutParams
+//                        initial.height = 90
+//                        TransitionManager.beginDelayedTransition(transitionsContainer)
+//                        visible = !visible
+//                        val v = if (visible) View.VISIBLE else View.GONE
+//                        val op = if (visible) View.GONE else View.VISIBLE
+//                        //viewEquipment.visibility = op
+//                        setVisibility(v)
+//                        val rotate = if (visible) 180f else 0f
+//                        dropDown.rotation = rotate
+//                        val params = equipmentLayout.layoutParams
+//                        val p = if (visible) 395 else 90
+//                        params.height = p
+//                    }
+//                }
+
+//                private fun recyclerAnimation() {
+//                    var visible = false
+//                    setVisibility(View.GONE)
+//                    val initial = equipmentLayout.layoutParams
+//                    initial.height = 90
+//                    dropDown.setOnClickListener{
+//                        TransitionManager.beginDelayedTransition(transitionsContainer)
+//                        visible = !visible
+//                        val v = if (visible) View.VISIBLE else View.GONE
+//                        val op = if (visible) View.GONE else View.VISIBLE
+//                        //viewEquipment.visibility = op
+//                        setVisibility(v)
+//                        val rotate = if (visible) 180f else 0f
+//                        dropDown.rotation = rotate
+//                        val params = equipmentLayout.layoutParams
+//                        val p = if (visible) 395 else 90
+//                        params.height = p
+//                    }
+//                }
+
+            }
             inner class NewFilter(var mAdapter: adapter) : Filter() {
                 override fun performFiltering(charSequence: CharSequence): FilterResults {
-                    country!!.clear()
+                    country.clear()
                     val results = FilterResults()
                     if (charSequence.length == 0) {
-                        country!!.addAll(equipmentList!!)
+                        country.addAll(equipmentList)
                     } else {
                         val filterPattern = charSequence.toString().toLowerCase().trim { it <= ' ' }
-                        for (listcountry in equipmentList!!) {
+                        for (listcountry in equipmentList) {
                             if (listcountry.toLowerCase().startsWith(filterPattern)) {
-                                country!!.add(listcountry)
+                                country.add(listcountry)
                             }
                         }
                     }
                     results.values = country
-                    results.count = country!!.size
+                    results.count = country.size
                     return results
                 }
 
@@ -171,6 +300,25 @@ class ChooseEquipment : RestApi() {
                 }
 
             }
+
+            //Switches the visibility of Equipment UI elements
+            private fun setVisibility(holder: adapter.myViewHolder, v: Int) {
+                holder.manufacturerText.visibility = v
+                holder.modelText.visibility = v
+                holder.locationText.visibility = v
+                holder.serialText.visibility = v
+                holder.lastServiceByText.visibility = v
+                holder.lastServiceDateText.visibility = v
+                holder.manufacturer.visibility = v
+                holder.modelNumber.visibility = v
+                holder.location.visibility = v
+                holder.serialNumber.visibility = v
+                holder.lastServiceBy.visibility = v
+                holder.lastServiceDate.visibility = v
+                holder.equipmentDivider.visibility = v
+            }
+
         }
+
 
     }
