ee516da8cf9f462fe18afc40ca84ebb2f2af3570
ksunjara <ksunny129@yahoo.com>
2020-03-09 21:11:30 -0700

    got rid of commented out code

--- Repairs/app/src/main/java/com/rooio/repairs/NavigationType.kt
+++ Repairs/app/src/main/java/com/rooio/repairs/NavigationType.kt
@@ -5,17 +5,4 @@ enum class NavigationType (private val value: String) {
     SETTINGS("SETTINGS"),
     EQUIPMENT("EQUIPMENT"),
     JOBS("JOBS");
-
-//    override fun toString(): String {
-////        return value
-////    }
-////
-////    fun getIntRepr(): Int {
-////        return when (this) {
-////            DASHBOARD -> 1
-////            SETTINGS -> 2
-////            EQUIPMENT -> 3
-////            JOBS -> 4
-////        }
-////    }
 }
