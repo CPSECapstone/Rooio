b21d0a7908c871c71ae9e24dd35f9069a3b41ec8
Samantha Koski <samanthakoski1@gmail.com>
2020-05-31 11:12:17 -0700

    made test file

---
+++ Repairs/app/src/test/java/com/rooio/repairs/JobDetailsTest.kt
@@ -0,0 +1,40 @@
+package com.rooio.repairs
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.volley.RequestQueue
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.robolectric.Robolectric
+
+@RunWith(AndroidJUnit4::class)
+class JobDetailsTest {
+
+    private lateinit var activity: JobDetails
+
+    @Mock
+    val queue: RequestQueue = Mockito.mock(RequestQueue::class.java)
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        RestApi.queue = queue
+        RestApi.userLocationID = "1"
+        activity = Robolectric.buildActivity(JobDetails::class.java)
+                .create()
+                .resume()
+                .get()
+    }
+
+    @Test
+    fun convertToNewFormat() {
+    }
+
+    @Test
+    fun convertToNewFormat2() {
+    }
+}
